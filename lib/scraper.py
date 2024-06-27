27651b620f7c8d96ddddf63d9c4e1e39a468f247
Paige <paigeberliner@gmail.com>
2024-06-27 07:13:31 -0400

    Complete Lab

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,17 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+courses = BeautifulSoup(html.text, 'html.parser')
+
+# Select elements with the combined class names
+selected_courses = courses.select('.heading-25-extrabold.color-black')
+
+# Print the selected elements (this will print the entire element as a list)
+print(selected_courses)
+
+# Iterate over the selected elements and print their text content
+for course in selected_courses:
+    print(course.get_text().strip())
